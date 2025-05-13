1e4f0317a4836703755b96a7082f3a8f94c66c0f
danimaschio6 <danielamaschio6@gmail.com>
2025-05-12 22:37:20 -0300

    encuestas.module correccion

--- src/modules/encuestas/encuestas.module.ts
+++ src/modules/encuestas/encuestas.module.ts
@@ -4,14 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Encuesta } from './entities/encuestas.entity';
 import { Pregunta } from './entities/pregunta.entity';
 import { Opcion } from './entities/opcion.entity';
+import { EncuestaController } from './controllers/encuestas.controllers';
 
 
 @Module({
     imports:[TypeOrmModule.forFeature([Encuesta,Pregunta,Opcion])],  //pasar entidades q vamos a utilizar en nuestro modulo
-    controllers: [EncuestasModule],
+    // controllers: [EncuestasModule],
+    controllers: [EncuestaController],
     providers: [EncuestasService],
 })
 
 export class EncuestasModule{
+    
 
 }
